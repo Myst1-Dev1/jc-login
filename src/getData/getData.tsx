a25bb89a00c9101d78d0e2ece217ba7399a09019
João Victor <joao.victor@internit.net.br>
2025-01-16 02:56:43 -0300

    .

--- src/getData/getData.tsx
+++ src/getData/getData.tsx
@@ -1,7 +1,11 @@
+import { jwtDecode } from "jwt-decode";
 
 export async function getData(token:string) {
+    const decoded:any = jwtDecode(token);
+
+    console.log(decoded);
     
-    const response = await fetch('https://homolog.kaindev.com.br/jc-atacados/api/users/2', {
+    const response = await fetch(`https://homolog.kaindev.com.br/jc-atacados/api/users/${decoded.id}`, {
         method: 'GET',
         headers: {
             "Content-Type": "application/json",
